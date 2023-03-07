5c0413082e685fe61ef0983b5158d530849c4b74
YasmimFahel <y.s.fahel.2002@outlook.com>
2023-03-07 07:46:24 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com?apikey=4e9502ab';
+const API_URL = 'https://www.omdbapi.com?apikey=4e9502ab';
 
 const App = () => {
 
